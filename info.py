fbd4bf6b2db2ee421763e5c7869b6dc6515bb38a
PLAYZHACKING <playzhacking12946@gmail.com>
2025-03-07 05:09:47 +0530

    info.py

--- info.py
+++ info.py
@@ -11,13 +11,13 @@ id_pattern = re.compile(r'^.\d+$')
 
 # Bot information
 SESSION = environ.get('SESSION', 'TechVJBot')
-API_ID = int(environ.get('API_ID', ''))
-API_HASH = environ.get('API_HASH', '')
+API_ID = int(environ.get('API_ID', '22161204'))
+API_HASH = environ.get('API_HASH', 'fdffc74281153b3338e4474f5640095e')
 BOT_TOKEN = environ.get('BOT_TOKEN', "")
 
 
 # This Pictures Is For Start Message Picture, You Can Add Multiple By Giving One Space Between Each.
-PICS = (environ.get('PICS', 'https://graph.org/file/ce1723991756e48c35aa1.jpg')).split()
+PICS = (environ.get('PICS', 'https://bit.ly/4ioV1V8')).split()
 
 
 # Admins & Users

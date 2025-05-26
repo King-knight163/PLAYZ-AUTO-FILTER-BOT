011282995645d9d013c49ec30fbdb3524aedb744
King-knight163 <crunchfun885@gmail.com>
2025-05-26 09:48:29 +0530

    commands.py

--- CloneTechVJ/commands.py
+++ CloneTechVJ/commands.py
@@ -44,7 +44,7 @@ async def start(client, message):
             up = cd["update_channel_link"]
             buttons.append([InlineKeyboardButton('🍿 ᴊᴏɪɴ ᴜᴘᴅᴀᴛᴇ ᴄʜᴀɴɴᴇʟ 🍿', url=up)])
         reply_markup = InlineKeyboardMarkup(buttons)
-        m=await message.reply_sticker("CAACAgUAAxkBAAEKVaxlCWGs1Ri6ti45xliLiUeweCnu4AACBAADwSQxMYnlHW4Ls8gQMAQ") 
+        m=await message.reply_sticker("CAACAgUAAxkBAAIC_Ggz61L_yjEYw394dK7JdOx10YwhAAKUEAACkZGYVQfNNNefDGgzHgQ") 
         await asyncio.sleep(1)
         await m.delete()
         await message.reply_text(
